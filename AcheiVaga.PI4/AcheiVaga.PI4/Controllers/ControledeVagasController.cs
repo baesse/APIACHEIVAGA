9607deb8258e0f3fd019d00f28da7a1960c4ba48
Igor Baesse de Araujo <igorbaesse86@gmail.com>
2017-04-01 18:06:11 -0300

    Primeira versão da api, foi criado os metodos de inclusão e busca das vagas em memoria, sem manipulação de banco.

---
+++ AcheiVaga.PI4/AcheiVaga.PI4/Controllers/ControledeVagasController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AcheiVaga.PI4.Controllers
+{
+    public class ControledeVagasController : ApiController
+    {
+        [HttpGet]
+        public List<Models.Vaga> GetVagas()
+        {
+            Models.Vaga vagas = new Models.Vaga();
+            return vagas.GetTodasasVagasOcupadas();
+        }
+
+
+        [HttpPut]
+        public string PutOcuparVaga(int id)
+        {
+            Models.Vaga vagas = new Models.Vaga();
+
+            vagas.PutRegistrarVaga(id);
+
+            return "Vaga";
+            
+        }
+
+    }
+}
---
+++ AcheiVaga.PI4/AcheiVaga.PI4/Models/Vaga.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcheiVaga.PI4.Models
+{
+    public  class Vaga
+    {
+        public int IdVaga { get; set; }
+        public bool VerOcupacao { get; set; }
+        public int IdEstacionamento { get; set; }
+        public static   List<Vaga> VagasOcupadas = new List<Vaga>();
+
+        public Vaga()
+        {
+           
+        }
+
+        public Vaga(int IdVaga,bool VerOcupacao,int IdEstacionamento)
+        {
+            this.IdVaga = IdVaga;
+            this.VerOcupacao = VerOcupacao;
+            this.IdEstacionamento = IdEstacionamento;
+        }
+
+
+        public List<Vaga> GetTodasasVagasOcupadas()
+        {
+            List<Vaga> ListadasOupadas = new List<Vaga>();
+            
+            
+            foreach(Vaga VagasDesocupadas in VagasOcupadas)
+            {
+                if (VagasDesocupadas.VerOcupacao != false)
+                {
+                    ListadasOupadas.Add(VagasDesocupadas);
+                }
+            }       
+
+           
+
+            return ListadasOupadas;
+            
+        }
+
+        public string PutRegistrarVaga(int id)
+        {
+
+
+            VagasOcupadas[1].VerOcupacao = true;
+            return "vaga ocupada";
+
+        }
+
+
+    }
+}
