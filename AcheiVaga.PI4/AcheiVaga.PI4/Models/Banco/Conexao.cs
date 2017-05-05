579e04875b1c4735062dccaa725fa2f16e13cc5d
Igor Baesse de Araujo <igorbaesse86@gmail.com>
2017-05-05 15:21:54 -0300

    Update Conexao.cs

--- AcheiVaga.PI4/AcheiVaga.PI4/Models/Banco/Conexao.cs
+++ AcheiVaga.PI4/AcheiVaga.PI4/Models/Banco/Conexao.cs
@@ -10,7 +10,7 @@ namespace AcheiVaga.PI4.Models.Banco
     {
         public static MongoClient cliente = new MongoClient("mongodb://localhost:27017");
         public static IMongoDatabase DataBase = cliente.GetDatabase("DBacheivaga");
-
+        // string conexao online public static MongoClient cliente = new MongoClient("mongodb://kay:75395146@cluster0-shard-00-02-hskmf.mongodb.net:27017/admin?ssl=true&authSource=root");
 
         
 
