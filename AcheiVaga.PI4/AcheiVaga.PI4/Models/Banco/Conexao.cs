cfdc1aacd5e470cce12c7655d700d466437165b9
Igor Baesse de Araujo <igorbaesse86@gmail.com>
2017-05-06 13:27:55 -0300

    Versão 1.0

--- AcheiVaga.PI4/AcheiVaga.PI4/Models/Banco/Conexao.cs
+++ AcheiVaga.PI4/AcheiVaga.PI4/Models/Banco/Conexao.cs
@@ -8,7 +8,9 @@ namespace AcheiVaga.PI4.Models.Banco
 {
     public static class Conexao
     {
-        public static MongoClient cliente = new MongoClient("mongodb://localhost:27017");
+        public static MongoClient cliente = new MongoClient("mongodb://root:75395146@cluster0-shard-00-00-hskmf.mongodb.net:27017,cluster0-shard-00-01-hskmf.mongodb.net:27017,cluster0-shard-00-02-hskmf.mongodb.net:27017/DBacheivaga?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+            //("mongodb://kay:75395146@cluster0-shard-00-02-hskmf.mongodb.net:27017/?ssl=true&authSource=admin");
+       // "mongodb://kay:75395146@cluster0-shard-00-00-hskmf.mongodb.net:27017,cluster0-shard-00-01-hskmf.mongodb.net:27017,cluster0-shard-00-02-hskmf.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin"
         public static IMongoDatabase DataBase = cliente.GetDatabase("DBacheivaga");
         // string conexao online public static MongoClient cliente = new MongoClient("mongodb://kay:75395146@cluster0-shard-00-02-hskmf.mongodb.net:27017/admin?ssl=true&authSource=root");
 
