4ee7306239664460887f91c9178bcf55793d1fe8
Igor Baesse de Araujo <igorbaesse86@gmail.com>
2017-05-08 20:34:01 -0300

    Update EstacionamentoController.cs

--- AcheiVaga.PI4/AcheiVaga.PI4/Controllers/EstacionamentoController.cs
+++ AcheiVaga.PI4/AcheiVaga.PI4/Controllers/EstacionamentoController.cs
@@ -24,5 +24,11 @@ namespace AcheiVaga.PI4.Controllers
             Estacionamento.CadastrarEstacionamento();
 
         }
+        [HttpGet]
+        public string retornar()
+        {
+            string "teste";
+
+        }
     }
 }
